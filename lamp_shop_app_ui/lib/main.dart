231becf9b42a356ffc367c676b76f85540c6d2ab
Grzesiek <gzaber@tlen.pl>
2022-09-24 14:51:31 +0200

    create files structure

--- lamp_shop_app_ui/lib/main.dart
+++ lamp_shop_app_ui/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lamp_shop_app_ui/pages/screens.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: const HomePage(),
     );
   }
 }
