644f141b517b0512f06be90088c799c38056b53e
Grzesiek <gzaber@tlen.pl>
2022-09-25 20:30:39 +0200

    develop details_page

--- lamp_shop_app_ui/lib/pages/details_page.dart
+++ lamp_shop_app_ui/lib/pages/details_page.dart
@@ -1,10 +1,53 @@
 import 'package:flutter/material.dart';
 
+import 'package:lamp_shop_app_ui/models/models.dart';
+
 class DetailsPage extends StatelessWidget {
-  const DetailsPage({Key? key}) : super(key: key);
+  const DetailsPage({
+    Key? key,
+    required this.lamp,
+  }) : super(key: key);
+
+  final Lamp lamp;
+
+  static Route route({required Lamp lamp}) {
+    return MaterialPageRoute(
+      builder: (context) => DetailsPage(lamp: lamp),
+      settings: const RouteSettings(name: '/details'),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.arrow_back),
+        ),
+        centerTitle: true,
+        title: const Text('Details'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.more_vert),
+          ),
+        ],
+      ),
+      extendBodyBehindAppBar: true,
+      body: Stack(
+        children: [
+          Image.network(
+            lamp.imageUrl,
+            width: width,
+            height: height / 2,
+            fit: BoxFit.cover,
+          ),
+        ],
+      ),
+    );
   }
 }
