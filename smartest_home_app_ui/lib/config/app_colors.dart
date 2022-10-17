df834a3154857133a7279dce2fde1330d3c72f5d
Grzesiek <gzaber@tlen.pl>
2022-10-17 22:07:01 +0200

    add grey color

--- smartest_home_app_ui/lib/config/app_colors.dart
+++ smartest_home_app_ui/lib/config/app_colors.dart
@@ -3,5 +3,6 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const brown = Color(0xFFB79980);
   static const darkBrown = Color(0xFF666158);
+  static const grey = Color(0xFFF2F2F2);
   static const orange = Color(0xFFFFBE77);
 }
