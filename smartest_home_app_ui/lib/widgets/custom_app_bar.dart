4c026524b22cd1045e9563229780388c10c0e9cb
Grzesiek <gzaber@tlen.pl>
2022-10-17 22:07:30 +0200

    develop widgets feature

---
+++ smartest_home_app_ui/lib/widgets/custom_app_bar.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
+  const CustomAppBar({
+    Key? key,
+    required this.height,
+    required this.leadingIcon,
+    this.trailingIcon,
+    this.leadingOnPressed,
+    this.trailingOnPressed,
+  }) : super(key: key);
+
+  final double height;
+  final IconData leadingIcon;
+  final IconData? trailingIcon;
+  final Function()? leadingOnPressed;
+  final Function()? trailingOnPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return PreferredSize(
+      preferredSize: Size.fromHeight(height),
+      child: Column(
+        children: [
+          const Spacer(),
+          AppBar(
+            titleTextStyle: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w400,
+            ),
+            iconTheme: const IconThemeData(
+              color: Colors.white,
+              size: 30,
+            ),
+            leadingWidth: 66,
+            toolbarHeight: height,
+            backgroundColor: Colors.transparent,
+            leading: Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: IconButton(
+                onPressed: leadingOnPressed ?? () {},
+                icon: Icon(leadingIcon),
+              ),
+            ),
+            title: const Text('Control Panel'),
+            actions: [
+              trailingIcon != null
+                  ? Padding(
+                      padding: const EdgeInsets.only(right: 10),
+                      child: IconButton(
+                        onPressed: trailingOnPressed ?? () {},
+                        icon: Icon(trailingIcon),
+                      ),
+                    )
+                  : const SizedBox(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(height);
+}
