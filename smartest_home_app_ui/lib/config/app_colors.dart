825f0da011a2c7c78a03824bd67f5108beadc783
Grzesiek <gzaber@tlen.pl>
2022-10-18 21:07:44 +0200

    update colors

--- smartest_home_app_ui/lib/config/app_colors.dart
+++ smartest_home_app_ui/lib/config/app_colors.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const brown = Color(0xFFB79980);
   static const darkBrown = Color(0xFF666158);
-  static const grey = Color(0xFFF2F2F2);
+  static const lightGrey = Color(0xFFF2F2F2);
+  static const grey = Color(0xFFA7A5A2);
   static const orange = Color(0xFFFFBE77);
 }
