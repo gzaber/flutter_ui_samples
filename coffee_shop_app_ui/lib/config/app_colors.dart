8720fcfd1266fae7167697d001a97905f47fb6ae
Grzesiek <gzaber@tlen.pl>
2022-11-05 16:58:56 +0100

    add app colors

---
+++ coffee_shop_app_ui/lib/config/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/widgets.dart';
+
+class AppColors {
+  static const black = Color(0xFF0C0F14);
+  static const lightBlack = Color(0xFF141921);
+  static const grey = Color(0xFF52555A);
+  static const orange = Color(0xFFD17842);
+}
