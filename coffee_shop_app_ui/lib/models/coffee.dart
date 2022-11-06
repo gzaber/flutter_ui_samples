42fcda76d116ab677438945c44af578fc0f9d8b1
Grzesiek <gzaber@tlen.pl>
2022-11-06 19:51:28 +0100

    update image urls

--- coffee_shop_app_ui/lib/models/coffee.dart
+++ coffee_shop_app_ui/lib/models/coffee.dart
@@ -27,7 +27,8 @@ List<Coffee> coffeeList = [
   Coffee(
     type: CoffeeType.cappuccino,
     size: CoffeeSize.small,
-    imageUrl: '',
+    imageUrl:
+        'https://images.unsplash.com/photo-1572442388796-11668a67e53d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=935&q=80',
     mainAddition: 'Oat Milk',
     price: 4.2,
     rating: 4.5,
@@ -36,7 +37,8 @@ List<Coffee> coffeeList = [
   Coffee(
     type: CoffeeType.cappuccino,
     size: CoffeeSize.small,
-    imageUrl: '',
+    imageUrl:
+        'https://images.unsplash.com/photo-1600056781444-55f3b64235e3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
     mainAddition: 'Chocolate',
     price: 4.5,
     rating: 4.7,
@@ -45,7 +47,8 @@ List<Coffee> coffeeList = [
   Coffee(
     type: CoffeeType.cappuccino,
     size: CoffeeSize.small,
-    imageUrl: '',
+    imageUrl:
+        'https://images.unsplash.com/photo-1474255710329-379550af928d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
     mainAddition: 'Cocoa',
     price: 4.1,
     rating: 4.3,
@@ -54,7 +57,8 @@ List<Coffee> coffeeList = [
   Coffee(
     type: CoffeeType.cappuccino,
     size: CoffeeSize.small,
-    imageUrl: '',
+    imageUrl:
+        'https://images.unsplash.com/photo-1468418143278-41595b1a4c89?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
     mainAddition: 'Cinnamon',
     price: 4.4,
     rating: 4.8,
