03dae1f931b041d2cb48a37b8f75946698d6c2db
Grzesiek <gzaber@tlen.pl>
2022-11-08 20:13:42 +0100

    change color

--- coffee_shop_app_ui/lib/widgets/custom_icon_button.dart
+++ coffee_shop_app_ui/lib/widgets/custom_icon_button.dart
@@ -11,47 +11,51 @@ class CustomAppBarButton extends StatelessWidget {
   }) : super(key: key);
 
   final IconData icon;
-  final Function onTap;
+  final Function() onTap;
   final bool isTransparent;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 35,
-      height: 35,
-      padding: const EdgeInsets.all(2),
-      decoration: BoxDecoration(
-        color: isTransparent ? Colors.transparent : null,
-        borderRadius: const BorderRadius.all(Radius.circular(10)),
-        gradient: !isTransparent
-            ? LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  const Color(0xFF262B33),
-                  const Color(0xFF262B33).withOpacity(0),
-                ],
-              )
-            : null,
-      ),
+    return GestureDetector(
+      onTap: onTap,
       child: Container(
-        width: double.infinity,
-        height: double.infinity,
-        decoration: const BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(10)),
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: [
-              Color(0xFF262B33),
-              Color(0xFF0C0F14),
-            ],
-          ),
+        width: 35,
+        height: 35,
+        padding: const EdgeInsets.all(2),
+        decoration: BoxDecoration(
+          color:
+              isTransparent ? const Color(0xFF262B33).withOpacity(0.7) : null,
+          borderRadius: const BorderRadius.all(Radius.circular(10)),
+          gradient: !isTransparent
+              ? LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    const Color(0xFF262B33),
+                    const Color(0xFF262B33).withOpacity(0),
+                  ],
+                )
+              : null,
         ),
-        child: Icon(
-          icon,
-          size: 20,
-          color: AppColors.grey,
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color(0xFF262B33),
+                Color(0xFF0C0F14),
+              ],
+            ),
+          ),
+          child: Icon(
+            icon,
+            size: 20,
+            color: AppColors.grey,
+          ),
         ),
       ),
     );
