e38501e3efb45cc100e32e5632619f48025844ca
Grzesiek <gzaber@tlen.pl>
2022-11-09 19:30:51 +0100

    rename coffee list to coffees

--- coffee_shop_app_ui/lib/models/coffee.dart
+++ coffee_shop_app_ui/lib/models/coffee.dart
@@ -3,7 +3,7 @@ import 'package:coffee_shop_app_ui/models/models.dart';
 enum CoffeeSize { small, medium, large }
 
 class Coffee {
-  Coffee({
+  const Coffee({
     required this.type,
     required this.size,
     required this.imageUrl,
@@ -23,12 +23,12 @@ class Coffee {
   final int reviewers;
 }
 
-List<Coffee> coffeeList = [
+const coffees = [
   Coffee(
     type: CoffeeType.cappuccino,
     size: CoffeeSize.small,
     imageUrl:
-        'https://images.unsplash.com/photo-1572442388796-11668a67e53d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=935&q=80',
+        'https://images.unsplash.com/photo-1512568400610-62da28bc8a13?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
     mainAddition: 'Oat Milk',
     price: 4.2,
     rating: 4.5,
@@ -48,7 +48,7 @@ List<Coffee> coffeeList = [
     type: CoffeeType.cappuccino,
     size: CoffeeSize.small,
     imageUrl:
-        'https://images.unsplash.com/photo-1474255710329-379550af928d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+        'https://images.unsplash.com/photo-1572097662444-003d63fe5884?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80',
     mainAddition: 'Cocoa',
     price: 4.1,
     rating: 4.3,
@@ -58,7 +58,7 @@ List<Coffee> coffeeList = [
     type: CoffeeType.cappuccino,
     size: CoffeeSize.small,
     imageUrl:
-        'https://images.unsplash.com/photo-1468418143278-41595b1a4c89?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
+        'https://images.unsplash.com/photo-1556124536-ed4da560eba8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
     mainAddition: 'Cinnamon',
     price: 4.4,
     rating: 4.8,
