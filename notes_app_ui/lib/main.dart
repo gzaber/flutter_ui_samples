cd1519a56ed097d3be873f07d006c30b39d21d86
Grzesiek <gzaber@tlen.pl>
2023-02-26 20:57:49 +0100

    initial commit

---
+++ notes_app_ui/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Notes App UI',
+      theme: ThemeData(
+        fontFamily: 'NeueMontreal',
+      ),
+      home: const HomePage(),
+    );
+  }
+}
