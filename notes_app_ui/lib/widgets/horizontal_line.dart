cd8b199dc72649934e462d240645ca5ae4c0446c
Grzesiek <gzaber@tlen.pl>
2023-03-27 19:10:54 +0200

    make HorizontalLine animated

--- notes_app_ui/lib/widgets/horizontal_line.dart
+++ notes_app_ui/lib/widgets/horizontal_line.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-class HorizontalLine extends StatelessWidget {
+import '../config/config.dart';
+
+class HorizontalLine extends StatefulWidget {
   const HorizontalLine({
     Key? key,
     required this.color,
@@ -9,11 +11,32 @@ class HorizontalLine extends StatelessWidget {
   final Color color;
 
   @override
+  State<HorizontalLine> createState() => _HorizontalLineState();
+}
+
+class _HorizontalLineState extends State<HorizontalLine> {
+  double width = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(milliseconds: 100)).then((_) {
+      setState(() {
+        width = MediaQuery.of(context).size.width - 50;
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 1,
-      color: color,
-      margin: const EdgeInsets.symmetric(horizontal: 25),
+    return Center(
+      child: AnimatedContainer(
+        width: width,
+        height: widget.color == AppColors.white ? 1 : 1.2,
+        color: widget.color,
+        margin: const EdgeInsets.symmetric(horizontal: 25),
+        duration: const Duration(seconds: 1),
+      ),
     );
   }
 }
